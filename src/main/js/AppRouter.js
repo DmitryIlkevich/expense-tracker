223d112aa3deff178d7c2b2ab7739cf3ebd4d88a
dmitryilkevich <dmitryilkievicz@gmail.com>
2023-11-23 21:14:50 +0100

    Added frontend router

---
+++ src/main/js/AppRouter.js
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import ListCategory from "./category/ListCategory";
+import TransactionHistory from "./history/TransactionHistory";
+
+/**
+ * Generates all available tabs to rout throw the application
+ */
+function CustomTabPanel(props) {
+    const {children, value, index, ...other} = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{p: 3}}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+CustomTabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+/**
+ * Renders category and transaction history tabs
+ */
+export default function BasicTabs() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Box sx={{width: '100%'}}>
+            <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                    <Tab label="Category" {...a11yProps(0)} />
+                    <Tab label="Transaction History" {...a11yProps(1)} />
+                </Tabs>
+            </Box>
+            <CustomTabPanel value={value} index={0}>
+                <ListCategory/>
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+                <TransactionHistory/>
+            </CustomTabPanel>
+        </Box>
+    );
+}
