a8bad3458f2f71ca36458b1df89e495ddebef283
dmitryilkevich <dmitryilkievicz@gmail.com>
2023-11-23 21:09:48 +0100

    Added app start class

---
+++ src/main/java/com/ui/expensetracker/ExpenseTrackerApplication.java
@@ -0,0 +1,17 @@
+package com.ui.expensetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Starts expense tracker application
+ *
+ * @author dmitryilkevich
+ */
+@SpringBootApplication
+public class ExpenseTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTrackerApplication.class, args);
+    }
+}
